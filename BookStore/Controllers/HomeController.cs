a124185a0e9fd5ef8fdbb721526ae2379b9a15fe
Sergey Korepanov <igzhestik@mail.ru>
2018-07-08 22:11:59 +0500

    Глава 3: Контроллеры. Переадресация и отправка кодов статуса и ошибок

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -22,6 +22,11 @@ namespace BookStore.Controllers
         [HttpGet]
         public ActionResult Buy(int id)
         {
+            if (id > 3)
+            {
+                return Redirect("/Home/Index");
+            }
+            
             ViewBag.BookId = id;
             return View();
         }
@@ -35,6 +40,7 @@ namespace BookStore.Controllers
             return $"Спасибо, {purchase.Person}, за покупку!";
         }
 
+        #region ActionResults
         public ActionResult GetHtml()
         {
             return new HtmlResult("<h2>Привет, мир!</h2>");
@@ -45,12 +51,57 @@ namespace BookStore.Controllers
             string path = "https://cdn.tproger.ru/wp-content/themes/bliss/assets/img/tp-circle2.png";
             return new ImageResult(path);
         }
+        #endregion ActionResults
 
+        #region ViewResult
         public ViewResult SomeMethod()
         {
             //ViewData["Head"] = "Привет, мир!";
             ViewBag.Head = "Привет, мир!";
             return View("SomeView");
         }
+        #endregion ViewResult
+
+        #region RedirectResult
+        public RedirectResult SomeRedirectMethod()
+        {
+            // временная переадресация
+            // считается, что запрашиваемый документ временно перемещен на другую страницу.
+            return Redirect("/Home/Index");
+
+            // постоянная переадресация
+            // считается, что запрашиваемый документ окончательно перемещен в другое место
+            // исп-ть нежелательно, т.к. браузер может автоматически
+            // настраивать запросы на новый ресурс, даже если старый ресурс
+            // со временем перестанет применять переадресацию.
+            //return RedirectPermanent("/Home/Index");
+        }
+
+        public RedirectToRouteResult SomeRTRMethod()
+        {
+            // перенаправление по определенному маршруту внутри домена
+            return RedirectToRoute(
+                new { controller = "Home", action = "Index" });
+        }
+
+        public RedirectToRouteResult SomeRTAMethod()
+        {
+            // перенаправление к определенному действию определенного контроллера
+            return RedirectToAction("Square", "Home",
+                new { a = 10, h = 12 });
+        }
+
+        public ActionResult CheckAge(int age)
+        {
+            if (age < 21)
+            {
+                //return new HttpStatusCodeResult(404);
+                return HttpNotFound();
+                //return HttpUnauthorizedResult();
+            }
+
+            return View();
+        }
+        #endregion RedirectResult
     }
 }
