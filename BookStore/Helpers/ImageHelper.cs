a1d8e232b9a75de5fe4231341081d4d5d3f1ac74
Sergey Korepanov <igzhestik@mail.ru>
2018-07-22 13:57:34 +0500

    Глава 4: Представления. HTML-хелперы

---
+++ BookStore/Helpers/ImageHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace BookStore.Helpers
+{
+    public static class ImageHelper
+    {
+        public static MvcHtmlString Image(this HtmlHelper html, string src, string alt)
+        {
+            TagBuilder img = new TagBuilder("img");
+            img.MergeAttribute("src", src);
+            img.MergeAttribute("alt", alt);
+
+            return MvcHtmlString.Create(img.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
---
+++ BookStore/Helpers/ListHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace BookStore.Helpers
+{
+    public static class ListHelper
+    {
+        public static MvcHtmlString CreateList(this HtmlHelper html, string[] items, object htmlAttributes = null)
+        {
+            TagBuilder ul = new TagBuilder("ul");
+
+            foreach (string item in items)
+            {
+                TagBuilder li = new TagBuilder("li");
+                li.SetInnerText(item);
+                ul.InnerHtml += li.ToString();
+            }
+
+            ul.MergeAttributes(HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+
+            return MvcHtmlString.Create(ul.ToString());
+        }
+    }
+}
